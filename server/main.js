6a058da887868839587022b165a85f3b256f86c8
손종국 <conquerex@gmail.com>
2018-05-15 17:45:32 +0900

    Mongoose and express session

--- server/main.js
+++ server/main.js
@@ -1,9 +1,12 @@
+import express from 'express';
+import path from 'path';
 import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
-// import express from 'express';
-// import path from 'path';
+import morgan from 'morgan';        // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser';   // PARSE HTML BODY
+import mongoose from 'mongoose';
+import session from 'express-session';
 
-const devPort = 4000;
 
 /**
  * Express Codes
@@ -22,8 +25,31 @@ app.listen(port, () => {
 });
 
 /**
+ * Middle ware
+ */
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const db = mongoose.connection;
+db.on(error, console.error);
+db.once('open', () => {
+    console.log('Connected to mongodb server');
+});
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/codelab');
+
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
+/**
  * Dev server
  */
+
+const devPort = 4000;
 
 if (process.env.NODE_ENV == 'development') {
     console.log('Server is running on dev mode!!');
