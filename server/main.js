dd67b7ad1d267acfa5f0cd7b0f5191c6a22cc58b
손종국 <conquerex@gmail.com>
2018-05-15 17:34:51 +0900

    webpack-dev-server connect setting

--- server/main.js
+++ server/main.js
@@ -1,6 +1,13 @@
-import express from 'express';
-import path from 'path';
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+// import express from 'express';
+// import path from 'path';
 
+const devPort = 4000;
+
+/**
+ * Express Codes
+ */
 const app = express();
 const port = 3000;
 
@@ -11,5 +18,21 @@ app.get('/hello', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('This app listening on port ', port);
+    console.log('This app is listening on port ', port);
 });
+
+/**
+ * Dev server
+ */
+
+if (process.env.NODE_ENV == 'development') {
+    console.log('Server is running on dev mode!!');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port ', devPort);
+        }
+    )
+}
