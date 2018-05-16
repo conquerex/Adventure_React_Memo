48f8cd6d2000dc781c61336f1e965b1928c8100f
unknown <conquerex@gmail.com>
2018-05-17 00:57:01 +0900

    signin POST method

--- server/routes/account.js
+++ server/routes/account.js
@@ -56,7 +56,54 @@ router.post('/signup', (req, res) => {
     });
 });
 
+/*
+    ACCOUNT SIGNIN: POST /api/account/signin
+    BODY SAMPLE: { "username": "test", "password": "test" }
+    ERROR CODES:
+        1: LOGIN FAILED
+*/
 router.post('/signin', (req, res) => {
+    if(typeof req.body.password !== "string") {
+        return res.status(401).json({
+            error: "LOGIN FAILED",
+            code: 1
+        });
+    }
+
+    // FIND THE USER BY USERNAME
+    Account.findOne({ username: req.body.username }, (err, account) => {
+        if (err) throw err;
+
+        // CHECK ACCOUNT EXISTANCY
+        if(!account) {
+            return res.status(401).json({
+                error: "LOGIN FAILED",
+                code: 1
+            });
+        };
+
+        // CHECK WHETHER THE PASSWORD IS VALID
+        if(!account.validateHash(req.body.password)) {
+            return res.status(401).json({
+                error: "LOGIN FAILED",
+                code: 1
+            });
+        };
+
+        // AFTER SESSION
+        let session = req.session;
+        session.loginInfo = {
+            _id: account._id,
+            username: acount.username
+        };
+
+        // RETURN SUCCESS
+        return res.json({
+            success: true
+        });
+    });
+
+
     /* to be implemented */
     res.json({success: true});
 });
