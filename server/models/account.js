4fe5ce2eba314f4241e751cc6e156b2c072f8e08
unknown <conquerex@gmail.com>
2018-05-17 00:21:50 +0900

    generates hash by bcryptjs

--- server/models/account.js
+++ server/models/account.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcryptjs from 'bcryptjs';
 
 const Schema = mongoose.Schema;
 
@@ -7,5 +8,16 @@ const Account = new Schema({
     password: String,
     created: { type: Date, default: Date.now}
 });
+
+
+// generates hash
+Account.method.generateHash = function(password) {
+    return bcryptjs.hashSync(password, 8);
+}
+
+// compares the password
+Account.method.validateHash = function(password) {
+    return bcryptjs.compareSync(password, this.password);
+}
 
 export default mongoose.model('account', Account);
