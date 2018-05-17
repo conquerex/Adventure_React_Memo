b235720451cff6d22dfe3d8a7f10de3b223a516a
unknown <conquerex@gmail.com>
2018-05-18 04:08:09 +0900

    Header component view

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,40 @@ import React, {Component} from 'react';
 
 class Header extends Component {
     render() {
+        
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper blue darken-1">
+                    <a className="brand-logo center">MEMOPAD</a>
+
+                    <ul>
+                        <li><a><i className="material-icons">search</i></a></li>
+                    </ul>
+
+                    <div className="right">
+                        <ul>
+                            <li>
+                                <a><i className="material-icons">vpn_key</i></a>
+                            </li>
+                            <li>
+                                <a><i className="material-icons">lock_open</i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
+
+Header.propTypes = {
+    isLoggedIn: React.PropTypes.bool,   // Login 상태인지 확인
+    onLogout: React.PropTypes.func      // 함수형 props, 로그아웃
+};
+
+Header.defaultProps = {
+    isLoggedIn: false,
+    onLogout: () => { console.log("Logout function not defined!!")}
+};
 
 export default Header;
