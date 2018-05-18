daf189e33ea5e8f5f84e06e42af08ee4890e66df
unknown <conquerex@gmail.com>
2018-05-19 03:42:03 +0900

    loginRequest used thunk

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,18 +1,29 @@
+import axios from 'axios';
 import {
     AUTH_LOGIN,
     AUTH_LOGIN_SUCCESS,
     AUTH_LOGIN_FAILURE
 } from './ActionTypes';
 
-
-
 /*============================================================================
     authentication
 ==============================================================================*/
 
-/* LOGIN */
-export function loginRequest() {
-    /* To be implemented */
+/* LOGIN use thunk */
+export function loginRequest(username, password) {
+    return (dispatch) => {
+        // Inform Login API is starting
+        dispatch(login());
+
+        // API REQUEST
+        return axios.post('/api/account/signin', { username, password} )
+            .then((response) => {
+                // SUCCEED
+                dispatch(loginSuccess(username));
+            }).catch((error) => {
+                dispatch(loginFailure());
+            })
+    }
 }
 
 export function login() {
