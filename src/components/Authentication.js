2930d30a5f75a53229775e36d5309b8ea71850ea
unknown <conquerex@gmail.com>
2018-05-19 01:21:43 +0900

    Dummy of Authentication component

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Authentication extends Component {
     render() {
+
+        const loginView = (
+            <div>loginView</div>
+        );
+
+        const registerView = (
+            <div>registerView</div>
+        );
+
         return (
-            <div>
-                Auth
+            <div className="container auth">
+                <Link className="logo" to="/">MEMOPAD</Link>
+                <div className="card">
+                    <div className="header blue white-text center">
+                        <div className="card-content">{this.props.mode ? "LOGIN" : "REGISTER"}</div>
+                    </div>
+                    {this.props.mode ? loginView : registerView}
+                </div>
             </div>
         );
     }
