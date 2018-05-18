f0c9cd7b7a2bf2c1a7468978bd195565713690ea
unknown <conquerex@gmail.com>
2018-05-19 03:56:25 +0900

    Refine Authentication reducer

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,5 +1,46 @@
-export default function authentication(state, action) {
-    if(typeof state === "undefined") state = {};
+import * as types from 'actions/ActionTypes';
+import update from 'react-addons-update';
 
-    return state;
+const initialState = {
+    login: {
+        status: 'INIT'
+    },
+    status: {
+        isLoggedIn: false,
+        currentUser: ''
+    }
+}
+
+export default function authentication(state, action) {
+    if(typeof state === "undefined")
+        state = initialState;
+
+    // thunk 를 리턴하는 loginRequest는 리듀서에서 따로 case를 지정해주지 않아도 됨
+    switch(action.type) {
+        /* LOGIN */
+        case types.AUTH_LOGIN:
+            return update(state, {
+                login: {
+                    status: { $set: 'WAITING' }
+                }
+            });
+        case types.AUTH_LOGIN_SUCCESS:
+            return update(state, {
+                login: {
+                    status: { $set: 'SUCCESS' }
+                },
+                status: {
+                    isLoggedIn: { $set: true },
+                    currentUser: { $set: action.username }
+                }
+            });
+        case types.AUTH_LOGIN_FAILURE:
+            return update(state, {
+                login: {
+                    status: { $set: 'FAILURE' }
+                }
+            });
+        default:
+            return state;
+    }
 }
