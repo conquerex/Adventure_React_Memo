c3e3a99c6afc21cd30c641f0572c25e8efbd5a39
unknown <conquerex@gmail.com>
2018-05-19 02:41:08 +0900

    Login action

--- src/index.js
+++ src/index.js
@@ -4,18 +4,32 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { App, Home, Login, Register } from 'containers';
 
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
 /* Check whether current route is login or register using regex */
 // let re = /(login|register)/;
 // let isAuth = re.test(this.props.location.pathname);
 
+/**
+ * How can I output the header from a specified component?
+ * https://goo.gl/aPDmJw
+ */
+
+const store = createStore(reducers, applyMiddleware(thunk));
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/home" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </div>
-    </Router>, rootElement
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/home" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </div>
+        </Router>
+    </Provider>, rootElement
 );
