86833d68c6d2f2ca719f7a559c6f92082b89433b
unknown <conquerex@gmail.com>
2018-05-20 07:17:55 +0900

    Refine registerRequest and add cases on auth reducer

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -52,8 +52,16 @@ export function loginFailure() {
 
 export function registerRequest(username, password) {
     return (dispatch) => {
-        //
-    }
+        // Inform Register API is starting
+        dispatch(register());
+
+        return axios.post('/api/account/signup', { username, password })
+            .then((response) => {
+                dispatch(registerSuccess());
+            }).catch((error) => {
+                dispatch(registerFailure(error.response.data.code));
+            });
+    };
 }
 
 export function register() {
